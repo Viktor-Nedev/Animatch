56f9eefc4fa06e9c6f0c5f39bf27431318d1a8d6
Viktor <MY_NAME@example.com>
2026-02-13 15:38:49 +0200

    Добавена е страница със списък на животни с контролер, изглед и навигационен линк.
    Създаден е AnimalController с Index за извеждане на животни и категории. Добавен е AnimalIndexViewModel. Обновен е _Layout.cshtml с линк към страницата.

---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,35 @@
+﻿using Animatch.Data;
+using Microsoft.AspNetCore.Mvc;
+using Animatch.Models;
+using Microsoft.EntityFrameworkCore;
+using Animatch.ViewModels;
+
+namespace Animatch.Controllers
+{
+	public class AnimalController : Controller
+	{
+
+		private readonly AnimalManagerDbContext context;
+
+
+		public AnimalController(AnimalManagerDbContext context)
+		{
+			this.context = context;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+
+			IEnumerable<Animal> animals = context
+				.Animals
+				.Include(animal => animal.Category)
+				.AsNoTracking()
+				.OrderBy(a => a.Name)
+				.ToList();
+
+
+			return View(animals);
+		}
+	}
+}
---
+++ ViewModels/AnimalIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Animatch.ViewModels.Animal
+{
+
+	public class AnimalIndexViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string Species { get; set; } = null!;
+		public string? Breed { get; set; }
+		public string? Town { get; set; }
+		public string CategoryName { get; set; } = null!;
+	}
+}
